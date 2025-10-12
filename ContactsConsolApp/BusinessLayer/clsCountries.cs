839a26c26204e7358532f650b2ddc66eabe57ef2
Faisal Alomari <faisalakalomari@gmail.com>
2025-10-12 12:55:34 +0300

    added clsCountries and clsDataAccessLayer updated

---
+++ ContactsConsolApp/BusinessLayer/clsCountries.cs
@@ -0,0 +1,48 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class clsCountries
+    {
+
+        public enum enMode { Add = 0, Update = 1 }
+
+        public enMode Mode = enMode.Add;
+
+        public int CountryID { set; get; }
+
+        public string CountryName { set; get; }
+        
+        public clsCountries()
+        {
+
+            this.CountryID = -1;
+            this.CountryName = "";
+        }
+
+        private clsCountries(int CountryID, string CountryName)
+        {
+
+            this.CountryID = CountryID;
+            this.CountryName = CountryName;
+
+            Mode = enMode.Update;
+        }
+
+        public static clsCountries Find(int CountryID)
+        {
+
+            string CountryName = "";
+
+            if (clsDataAccessLayer.FindCountryByID(CountryID, ref CountryName))
+                return new clsCountries(CountryID, CountryName);
+            else
+                return null;
+        }
+    }
+}
